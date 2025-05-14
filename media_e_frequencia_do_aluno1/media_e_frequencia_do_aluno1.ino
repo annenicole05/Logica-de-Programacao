a1db70a5aa1445ce0632990142f2965b046846e8
Anne <annenicole979@gmail.com>
2025-05-14 10:03:26 -0300

    adicionado exercicio de frequencia

---
+++ media_e_frequencia_do_aluno1/media_e_frequencia_do_aluno1.ino
@@ -0,0 +1,27 @@
+// Avaliação de Desempenho Escolar
+//
+
+ int nota = 6 && 8;
+ int frequencia = 75
+
+ Serial.begin(9600);
+
+ Serial.println("Parabens voce foi aprovado!!");
+Serial.println();
+
+{
+  void loop()
+}   
+  Serial.println("Soma das notas " + String(frequencias) ");
+  
+ 
+ if(frequencia >0 &&  <=75){
+  Serial.println("apenas frequencia acima de 75");
+ } else if(nota >= 6 && nota< 8){
+   Serial.println("
+
+ 
+void loop()
+{
+  
+}
---
+++ spectacular_lahdi_snicket1/spectacular_lahdi_snicket1.ino
@@ -0,0 +1,39 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Escolha uma fruta");
+  Serial.println("   1 - Banana");
+  Serial.println("   2 - maça");
+  Serial.println("   3 - Tomate");
+  Serial.println("   4 - Laranja");
+  while(!Serial.available());
+  int opcao = Serial.parseInt();
+  
+  switch(opcao)
+  {
+    case 1:
+        Serial.println("Opcao escolhida: Banana");
+    break;
+    case 2 :
+        Serial.println("Opcao escolhida: Maca");
+    break;
+    case 3 :
+        Serial.println("Opcao escolhida: Toamte");
+    break;
+    case 4 :
+        Serial.println("Opcao escolhida: Laranja");
+    break;
+    default:
+         Serial.println("Nao temos essa  opcao");
+  }
+  
+   Serial.println();
+  
+}// fim do loop
+      
